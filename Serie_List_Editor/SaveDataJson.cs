2059cfa3b18401a545034374a34ad2ad7f8745e3
karlo <k.koelewijn2@gmail.com>
2020-01-28 16:23:12 +0100

    bug fix

--- Serie_List_Editor/SaveDataJson.cs
+++ Serie_List_Editor/SaveDataJson.cs
@@ -7,14 +7,29 @@ using System.Threading.Tasks;
 namespace Serie_List_Editor
 {
     [Serializable]
-    internal struct SaveDataJson
+    internal class SaveDataJson
     {
+        public SaveDataJson()
+        {
+        }
+
+        public SaveDataJson(bool meh)
+        {
+            if (meh)
+            {
+                Title = new List<string>();
+                Episode = new List<int?>();
+                Season = new List<int?>();
+                Note = new List<string>();
+            }
+        }
+
         public List<string> Title;
         public List<int?> Episode;
         public List<int?> Season;
         public List<string> Note;
 
-        public void AddNewEntry(string title = "Title", int season = 0, int episode = 0, string note = "Empty note")
+        public void AddNewEntry(string title = "Title", int season = 1, int episode = 1, string note = "Empty note")
         {
             Title.Add(title);
             Season.Add(season);
